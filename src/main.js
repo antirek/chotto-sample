bc28a1d140bd02792d157b1c274667763e12cb78
sergey <110@mobilon.ru>
2024-09-18 17:07:52 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import App from './App.vue'
 
 import { createPinia } from 'pinia'
 import '/node_modules/@mobilon-dev/chotto/dist/style.css';
-const pinia = createPinia()
 
+const pinia = createPinia()
 const app = createApp(App);
+
 app.use(pinia)
 app.mount('#app')
